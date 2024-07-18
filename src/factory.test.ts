455139a348b4ff43c131fa1349b2c0e9b1196409
sandadi Sreenu <sreenu.friends18@gmail.com>
2024-07-18 10:10:51 +0530

    Test cases: updated test cases and description

--- src/factory.test.ts
+++ src/factory.test.ts
@@ -13,13 +13,13 @@ test('creates a count starting from 10 with a step of 5', function () {
   expect(count()).toBe(20);
 });
 
-test('create count with negative start value from -10 with a step of 5', function () {
+test('create a count with negative start value from -10 with a step of 5', function () {
   const count = factory(-10, 5);
   expect(count()).toBe(-5);
   expect(count()).toBe(0);
 });
 
-test('create count with negative step value from 10 with a step of -5', function () {
+test('create a count with negative step value from 10 with a step of -5', function () {
   const count = factory(10, -5);
   expect(count()).toBe(5);
   expect(count()).toBe(0);
@@ -31,7 +31,7 @@ test('defaults to start 0, step 1 when no arguments passed', function () {
   expect(count()).toBe(2);
 });
 
-test('create count with large step value', function () {
+test('create a count with large step value', function () {
   const count = factory(0, 1000);
   expect(count()).toBe(1000);
   expect(count()).toBe(2000);
@@ -43,8 +43,14 @@ test('handle the count with negative values', function () {
   expect(count()).toBe(-25);
 });
 
-test('create the count with float values', function () {
+test('create a count with float values', function () {
   const count = factory(0.25, 0.25);
   expect(count()).toBe(0.5);
   expect(count()).toBe(0.75);
+});
+
+test('create a count with step value is 0', function () {
+  const count = factory(10, 0);
+  expect(count()).toBe(10);
+  expect(count()).toBe(10);
 });
